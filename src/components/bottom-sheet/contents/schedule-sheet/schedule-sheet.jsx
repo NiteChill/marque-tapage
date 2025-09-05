a9817bafa21d531c3e47450eedf8b552def12c29
NiteChill <puissantachille@gmail.com>
2025-09-05 22:03:29 +0200

    schedule done quickly

--- src/components/bottom-sheet/contents/schedule-sheet/schedule-sheet.jsx
+++ src/components/bottom-sheet/contents/schedule-sheet/schedule-sheet.jsx
@@ -1,4 +1,7 @@
+import { ChevronRight } from '../../../icons/icons';
 import s from './schedule-sheet.module.scss';
+import phoneIcon from '../../../../assets/img/phone-icon.svg'
+import mailIcon from '../../../../assets/img/mail-icon.svg';
 
 export const ScheduleSheet = () => {
   return (
@@ -8,6 +11,40 @@ export const ScheduleSheet = () => {
         L'horaire ci dessous peut s'avérer incorrect dans certaine conditions
         exceptionnelles.
       </p>
+      <div className={s.schedule}>
+        <ScheduleLine label='Lundi' hours='Fermé' />
+        <ScheduleLine label='Mar. - Ven.' hours='10:00 - 18:00' />
+        <ScheduleLine label='Samedi' hours='10:00 - 17:00' />
+        <ScheduleLine label='Dimanche' hours='Fermé' />
+      </div>
+      <h4 className='title-large'>Contact</h4>
+      <div className={s.contacts}>
+        <ContactsLine src={phoneIcon} alt='Phone'>
+          +32 497 12 15 24
+        </ContactsLine>
+        <ContactsLine src={mailIcon} alt='Mail'>
+          marquetapage@gmail.com
+        </ContactsLine>
+      </div>
     </main>
+  );
+};
+
+const ScheduleLine = ({ label, hours }) => {
+  return (
+    <div className={s.schedule_line}>
+      <p className='title-medium'>{label}</p>
+      <p className='body-large'>{hours}</p>
+    </div>
+  );
+};
+
+const ContactsLine = ({ children, src, alt, onClick }) => {
+  return (
+    <div className={s.contacts_line} onClick={onClick}>
+      <img src={src} alt={alt} />
+      <p className='title-medium'>{children}</p>
+      <ChevronRight />
+    </div>
   );
 };
