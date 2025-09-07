74182ba81e423c3d896da5527e9052d7af76f496
NiteChill <puissantachille@gmail.com>
2025-09-07 18:22:52 +0200

    search history UI done, need to implement the cookie logic.

--- src/components/bottom-sheet/contents/search-sheet/search-sheet.jsx
+++ src/components/bottom-sheet/contents/search-sheet/search-sheet.jsx
@@ -1,3 +1,4 @@
+import { History } from '../../../icons/icons';
 import { SearchInput } from '../../../search-input/search-input';
 import s from './search-sheet.module.scss';
 
@@ -8,7 +9,26 @@ export const SearchSheet = () => {
       <p className='body-medium'>
         Cherchez, réservez ou commandez un livre grâce au site Librel.
       </p>
-      <SearchInput></SearchInput>
+      <SearchInput />
+      <div className={s.history_title}>
+        <p className='label-large'>Recherches récentes</p>
+        <button className='title-small'>Gérer</button>
+      </div>
+      <div className={s.history}>
+        <HistoryLine>Spiderman</HistoryLine>
+        <span className={s.divider} />
+        <HistoryLine>Harry Potter</HistoryLine>
+      </div>
     </main>
+  );
+};
+
+const HistoryLine = ({ children }) => {
+  const encodedValue = encodeURIComponent(children);
+  return (
+    <a target='_blank' href={`https://www.librel.be/listeliv.php?flou&mots_recherche=${encodedValue}&base=allbooks`} className={s.history_line}>
+      <History className={s.icon} />
+      <p className='body-large'>{children}</p>
+    </a>
   );
 };
