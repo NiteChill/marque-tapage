59971d79a8b7a330fa0d6de09744483464f3ee17
NiteChill <puissantachille@gmail.com>
2025-09-08 03:28:13 +0200

    UI for search history done, still need to code the logic.

--- src/components/bottom-sheet/contents/search-sheet/search-sheet.jsx
+++ src/components/bottom-sheet/contents/search-sheet/search-sheet.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import { History } from '../../../icons/icons';
+import { Delete, History } from '../../../icons/icons';
 import { SearchInput } from '../../../search-input/search-input';
 import s from './search-sheet.module.scss';
 import Cookies from 'js-cookie';
 import { TextButton } from '../../../text-button/text-button';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 export const SearchSheet = () => {
   const [searchHistory, setSearchHistory] = useState([]),
@@ -12,6 +14,22 @@ export const SearchSheet = () => {
     const h = Cookies.get('marque-tapage-search-history');
     if (h) setSearchHistory(JSON.parse(h));
   }, []);
+  useGSAP(() => {
+    gsap.fromTo(
+      '#search-history-delete-button',
+      {
+        width: edit ? 0 : 56,
+        scale: edit ? 0.75 : 1,
+      },
+      {
+        width: edit ? 56 : 0,
+        scale: edit ? 1 : 0.75,
+        ease: 'elastic(1,1)',
+        duration: 0.4,
+        stagger: 0.02,
+      }
+    );
+  }, [edit]);
   return (
     <main className={s.search_sheet}>
       <h2 className='headline-large'>Rechercher</h2>
@@ -31,8 +49,8 @@ export const SearchSheet = () => {
       <div className={s.history}>
         {searchHistory.map((el, id) => (
           <div key={crypto.randomUUID()}>
-            {id === 0 && <span className='divider' />}
-            <HistoryLine>{el}</HistoryLine>
+            {id !== 0 && <span className={s.divider} />}
+            <HistoryLine edit={edit}>{el}</HistoryLine>
           </div>
         ))}
       </div>
@@ -40,16 +58,20 @@ export const SearchSheet = () => {
   );
 };
 
-const HistoryLine = ({ children }) => {
+const HistoryLine = ({ children, edit }) => {
   const encodedValue = encodeURIComponent(children);
   return (
-    <a
-      target='_blank'
-      href={`https://www.librel.be/listeliv.php?flou&mots_recherche=${encodedValue}&base=allbooks`}
-      className={s.history_line}
-    >
-      <History className={s.icon} />
-      <p className='body-large'>{children}</p>
-    </a>
+    <div className={s.history_line}>
+      <a
+        target='_blank'
+        href={`https://www.librel.be/listeliv.php?flou&mots_recherche=${encodedValue}&base=allbooks`}
+      >
+        <History className={s.icon} />
+        <p className='body-large'>{children}</p>
+      </a>
+      <button tabIndex={edit ? 0 : -1} className={s.icon_button} id='search-history-delete-button'>
+        <Delete className={s.icon} />
+      </button>
+    </div>
   );
 };
